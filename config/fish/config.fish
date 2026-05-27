8e9c61acebabad76bf2804c8f834c7df758eae86
Alexander Arrico <alex@arrico.me>
2026-05-27 12:02:46 -0400

    fix syntax errors and setting adjustments

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,18 +13,19 @@ if status is-interactive
     else
         alias ll="ls -lh --color=auto"
         alias la="ls -lah --color=auto"
-    fi
+    end 
     alias g="git"
-	alias gaa="git add ."
-	alias gp="git push"
-	alias gl="git pull"
-	alias gst="git status"
-	alias gd="git diff"
+    alias gaa="git add ."
+    alias gp="git push"
+    alias gl="git pull"
+    alias gst="git status"
+    alias gd="git diff"
     alias dfu="dotfiles_update"
-	alias v="nvim"
-	alias lg="lazygit"
-	alias cat="bat --style=plain"
-
+    alias v="nvim"
+    alias lg="lazygit"
+    alias cat="bat --style=plain"
+    alias cheat="xdg-open \$dotfiles_repo_dir/index.html"
+    
     # --- Heavy Engine Interventions: Advanced FZF ---
     set -gx FZF_DEFAULT_OPTS "--height 45% --layout=reverse --border=rounded --inline-info --color='pointer:#7dcfff,hdr:#bb9af7,info:#e0af68,marker:#9ece6a'"
     set -gx fzf_fd_opts --hidden --exclude .git --exclude node_modules --exclude .cache
